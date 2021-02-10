e20dc56a2adb37bfde04895eb6cf3c7e1373d880
Anilton Cardoso <acardosoj@gmail.com>
2021-02-10 13:32:06 -0300

    Adding B3 extractor adding random wait in ds Manager adding file handler

--- Extractors/DSManager.py
+++ Extractors/DSManager.py
@@ -1,4 +1,6 @@
 import pandas as pd 
+import numpy as np
+from tqdm import tqdm
 
 
 class Manager: 
@@ -8,3 +10,9 @@ class Manager:
 
     def append_data(self, df):
         pass
+
+    def random_wait(self):
+        wait_times = [0.2, 0.5, 1, 2]
+        probs = [0.3, 0.4, 0.2, 0.1 ]
+        choice = np.random.choice(wait_times, size=1, p=probs)
+        return choice
