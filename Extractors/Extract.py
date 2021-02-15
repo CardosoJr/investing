5e866751c8a74525193f8ce742b7f5c2ac9015de
Anilton Cardoso <acardosoj@gmail.com>
2021-02-14 22:46:08 -0300

    creating extract notebook as an example improving the extract methods

--- Extractors/Extract.py
+++ Extractors/Extract.py
@@ -11,7 +11,17 @@ from .DSManager import Manager
 from pathlib import Path
 from tqdm import tqdm
 
+def build_full_assets(path_dir, fundamentos):
+    path = Path(path_dir)
+    segments = pd.read_csv(Path('./Extractors/b3/segments.csv'))
+    segments = segments[['Setor', 'Subsetor', 'Codigo']]
+    fundamentos['Codigo'] = fundamentos['Ticker'].str[:4]
+
+    asset_df = pd.merge(left = segments, right = fundamentos, on = "Codigo", how = 'left')
+    asset_df.to_csv(path / 'asset_df', index = False)
+
 def build_fundamentos(path_dir):
+    print('Building Fundamentus data\n')
     path = Path(path_dir)
     tickers = fundamentus.get_tickers()
     tickers.rename(columns = {"Papel":"Ticker"}, inplace = True)
@@ -25,22 +35,15 @@ def build_fundamentos(path_dir):
             sleep(sleep_time)
             fundamentos = fundamentos.append(df)
         except: 
-            print("error in trying to get data from ticker", ticker)
-            ticker_errors.appendq(ticker)
+            print("\nerror in trying to get data from ticker", ticker)
+            ticker_errors.append(ticker)
         
 
     fundamentos = pd.merge(right = tickers, left = fundamentos, on = "Ticker", how = 'left')
     fundamentos.to_csv(path / "fundamentos.csv", index = False)
 
     with open(path / "log.txt", 'w') as f:
-        f.writelines(ticker_errors)
-
-    segments = pd.read_csv('./Extractors/b3/segments.csv')
-    segments = segments[['Setor', 'Subsetor', 'Codigo']]
-    fundamentos['Codigo'] = fundamentos['Ticker'].str[:4]
-
-    asset_df = pd.merge(left = segments, right = fundamentos, on = "Codigo", how = 'left')
-    asset_df.to_csv(path / 'asset_df', index = False)
+        f.writelines('\n'.join(ticker_errors))
 
     return fundamentos
 
@@ -48,4 +51,4 @@ def random_wait():
     wait_times = [0.2, 0.5, 1, 2]
     probs = [0.3, 0.4, 0.2, 0.1 ]
     choice = np.random.choice(wait_times, size=1, p=probs)
-    return choice
+    return choice[0]
