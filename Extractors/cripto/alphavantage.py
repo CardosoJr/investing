525e09b0c9b71ece49c87c3db102b689b4dc9e2b
Anilton Cardoso <acardosoj@gmail.com>
2021-02-16 20:41:48 -0300

    fixing some stuff in alphavantage.py

--- Extractors/cripto/alphavantage.py
+++ Extractors/cripto/alphavantage.py
@@ -25,15 +25,17 @@ class Alphavantage:
             raise Exception("past_months should be between 1 and 24")
         query = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol={}&interval={}&slice={}&apikey={}".format(
             symbol, self.Interval[interval_min], self.__convert_months(past_months), self.Key)
+        print(query)
         data = requests.get(query).content
         result = pd.read_csv(io.StringIO(data.decode('utf-8')))
         return result
 
     def get_day_data(self, symbol, interval_min):
-        query = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={}&interval={}&apikey={}".format(
+        query = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={}&interval={}&outputsize=full&datatype=csv&apikey={}".format(
             symbol, self.Interval[interval_min], self.Key)
-        data = requests.get(query).json()
-        result = pd.DataFrame.from_dict(data[f"Time Series ({self.Interval[interval_min]})"], orient = 'index')
+        print(query)
+        data = requests.get(query).content
+        result = pd.read_csv(io.StringIO(data.decode('utf-8')))
         return result
 
     def get_cripto_data(self, symbol, market):
