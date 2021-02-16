d2f094be6022a6fed928da0a08e04fc1d1b3639b
Anilton Cardoso <acardosoj@gmail.com>
2021-02-16 14:17:18 -0300

    fixing some bugs

--- Extractors/Extract.py
+++ Extractors/Extract.py
@@ -18,33 +18,34 @@ def build_full_assets(path_dir, fundamentos):
     fundamentos['Codigo'] = fundamentos['Ticker'].str[:4]
 
     asset_df = pd.merge(left = segments, right = fundamentos, on = "Codigo", how = 'left')
-    new_columns = [(x.split("'")[-1] if len(x.split("'")) == 1 else (x.split("'")[-2] if x.split("'")[-2] != "" else x.split("'")[1])) for x in asset_df.columns.ravel()]
-    asset_df.columns = new_columns
-    asset_df = asset_df.loc[:,~asset_df.columns.duplicated()] 
-    asset_df.to_csv(path / 'asset_df', index = False)
+    asset_df.to_csv(path / 'assets.csv', index = False)
     return asset_df
 
 def get_fundamentos_simple():
     return pd.DataFrame.from_dict(fundamentus.get_data(), orient = 'index')
      
 
-def build_fundamentos_tickers(path_dir, tickers):
+def build_fundamentos_tickers(path_dir, tickers, year = None, quarter = None):
     print('Building Fundamentus data\n')
     path = Path(path_dir)
     fundamentos = pd.DataFrame([])
     ticker_errors = []
     for ticker in tqdm(tickers):
         try:
-            df = advfn.get_fundamentos(ticker)
+            df = advfn.get_fundamentos(ticker, year = year, quarter = quarter)
             sleep_time = random_wait()
             sleep(sleep_time)
             fundamentos = fundamentos.append(df)
         except: 
             print("\nerror in trying to get data from ticker", ticker)
             ticker_errors.append(ticker)
-        
+
+    fundamentos.to_csv(path / "bkp_fund.csv", index = False)   
 
     # fundamentos = pd.merge(right = tickers, left = fundamentos, on = "Ticker", how = 'left')
+    new_columns = [x[-1] if x[-1] != "" else x[0] for x in fundamentos.columns.ravel()]
+    fundamentos.columns = new_columns
+    fundamentos = fundamentos.loc[:,~fundamentos.columns.duplicated()] 
     fundamentos.to_csv(path / "fundamentos.csv", index = False)
 
     with open(path / "log.txt", 'w') as f:
