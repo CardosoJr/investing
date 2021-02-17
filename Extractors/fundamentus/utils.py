c6e0e5e60016c095c0b39478668a6b18e8dd6b5b
Anilton Cardoso <acardosoj@gmail.com>
2021-02-17 14:25:34 -0300

    adding some missing files

---
+++ Extractors/fundamentus/utils.py
@@ -0,0 +1,13 @@
+class DataNotFound(IndexError):
+    pass
+
+
+def convert_type(x):
+    if str(x).endswith('%'):
+        return float(x[:-1].replace('.', '').replace(',', '.')) / 100
+    try:
+        if float(x).is_integer():
+            return int(float(x))
+        return float(x)
+    except ValueError:
+        return None
