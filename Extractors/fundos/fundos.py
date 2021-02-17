8732b330f95af05296f97315e620bfe4e3c07a5d
Anilton Cardoso <acardosoj@gmail.com>
2021-02-16 22:47:39 -0300

    improving fundos extractor

--- Extractors/fundos/fundos.py
+++ Extractors/fundos/fundos.py
@@ -4,29 +4,39 @@ from decimal import Decimal
 import pandas as pd
 from dateutil.relativedelta import relativedelta
 from datetime import datetime
+from tqdm import tqdm
 
 from pandas.core import base
 
-def get_data(start_date, end_date, filter = None):
-  base_url = "http://dados.cvm.gov.br/dados/FI/DOC/INF_DIARIO/DADOS/inf_diario_fi_[ANOMES].csv"
+def get_info_cadastral():
+  #http://dados.cvm.gov.br/dataset/fi-cad
+  #http://dados.cvm.gov.br/dados/FI/CAD/DADOS/cad_fi.csv
+  base_url = "http://dados.cvm.gov.br/dados/FI/CAD/DADOS/cad_fi.csv"
+  df = pd.read_csv(base_url, sep = ';', encoding = "ISO-8859-1")
+  return df
 
+
+def get_data(start_date, end_date, filter = None, min_cot = None):
+  base_url = "http://dados.cvm.gov.br/dados/FI/DOC/INF_DIARIO/DADOS/inf_diario_fi_[ANOMES].csv"
 
   initial = datetime.strptime(start_date, "%Y-%m-%d")
   final = datetime.strptime(end_date, "%Y-%m-%d") 
   
   anomes = []
   while initial <= final:
-      week = initial.strftime("%Y%W")
       anomes.append(initial.strftime("%Y%m"))
       initial = initial + relativedelta(months = 1)
 
   result = pd.DataFrame([])
-  for am in anomes:
+  for am in tqdm(anomes):
     url = base_url.replace("[ANOMES]", am)
-    df = pd.read_csv(url)
+    df = pd.read_csv(url, sep = ';')
     
     if filter is not None:
       df = df[df['CNPJ_FUNDO'].isin(filter)]
+
+    if min_cot is not None:
+      df = df[df["NR_COTST"] >= min_cot]
 
     result = result.append(df)
 
