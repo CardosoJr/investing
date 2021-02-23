4860c2ae6fd619665864d1ae1f5bfe5ac52ab66a
Anilton Cardoso <acardosoj@gmail.com>
2021-02-23 08:02:53 -0300

    Portfolio tree map

--- Analysis/analysis.py
+++ Analysis/analysis.py
@@ -1,6 +1,13 @@
 import pandas as pd 
 import numpy as np
 from pathlib import Path
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import squarify # pip install squarify (algorithm for treemap)&lt;/pre&gt;
+import seaborn as sns 
+
+
 
 class FinanceAnalysis:
     def __init__(self, path_dir):
@@ -9,4 +16,29 @@ class FinanceAnalysis:
         
     def daily_view(self):
         pass
+
+    def portfolio_treemap(self, data, performance, labels):
+        # Create a dataset:
+        my_values=[i**3 for i in range(1,100)]
+        
+        # create a color palette, mapped to these values
+        cmap_reds = matplotlib.cm.Reds
+        mini=min(my_values)
+        maxi=max(0.0)
+        norm_reds = matplotlib.colors.Normalize(vmin=mini, vmax=maxi)
+
+        cmap_greens = matplotlib.cm.Greens
+        mini=min(0.0)
+        maxi=max(my_values)
+        norm_greens = matplotlib.colors.Normalize(vmin=mini, vmax=maxi)
+
+        colors = [cmap_reds(norm_reds(value)) if x <= 0 else cmap_greens(norm_greens(value))  for value in my_values]
+        
+        labels = [x + "\n" + str(performance[i]) for x,i in enumerate(labels)]
+
+        # Change color
+        squarify.plot(sizes=my_values, labels = labels, alpha=.8, color=colors )
+        plt.axis('off')
+        plt.show()
+
     
