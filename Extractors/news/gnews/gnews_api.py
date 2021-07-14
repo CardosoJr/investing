3e4650493bdd108fa3ef8742f54c42523e2130f2
Anilton Cardoso <acardosoj@gmail.com>
2021-07-13 23:47:37 -0300

    starting gnews extractor3

---
+++ Extractors/news/gnews/gnews_api.py
@@ -0,0 +1,77 @@
+from re import A
+from gnews import GNews
+from GoogleNews import GoogleNews
+
+import pandas as pd 
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+
+class NewsExtractor: 
+    def __init__(self):
+        self.now = datetime.now()
+    
+    def extract_history(self, from_date, to_date, filters, max_pages = 10, max_itens = 100, country = "BR", language = "PT", extract_full = False):
+        date_intervals = []
+        current = from_date 
+        while current < to_date:
+            final = current + relativedelta(days = 30)
+            if final > to_date:
+                final = to_date
+            date_intervals.append((current, final))
+            current = current + relativedelta(days = 31)
+
+        data = []
+
+        for dt_interval in date_intervals:
+            googlenews = GoogleNews(lang = 'pt', start = dt_interval[0].strftime("%d/%m/%Y"), end = dt_interval[1].strftime("%d/%m/%Y"))
+            googlenews.search(filters)
+            
+            num_page = 1
+            titles = []
+
+            while len(titles) < max_itens and num_page < max_pages:
+                googlenews.get_page(num_page)
+                results = googlenews.result()
+
+                df = pd.DataFrame(results)
+                df = df[~df['title'].isin(titles)]
+                df = df[['title', 'datetime', 'desc', 'link', 'media']]
+
+                df.columns = ['title', 'published date', 'description', 'url', 'publisher']
+                titles.extend(df['title'])
+                num_page += 1
+                data.append(df)
+        final_result = pd.DataFrame([])
+        if len(data) > 0:
+            final_result = pd.concat(data, ignore_index = True)
+        
+        return final_result
+
+    def extract_daily(self, from_date, filters, country = "BR", language = "PT", extract_full = False):
+        delta = (self.now - from_date).days
+
+        if delta > 30:
+            raise Exception("Period too large. Use Method extract_history instead")
+
+        period = "" + delta + "d"
+        gnews = GNews(language = language, country = country, period = period)
+        
+        result = gnews.get_news(filters)
+        new_result = []
+        for x in result:
+            x['publisher'] = x['publisher']['title']
+            x['title'] = x['title'].replace(" - " + x['publisher'], "")
+            new_result.append(x)
+
+        result = new_result
+
+
+        if extract_full:
+            for article in result:
+                url = article['url']
+                text = gnews.get_full_article(url)
+                article['full_text'] = text
+
+        df = pd.DataFrame(result)
+        return df
